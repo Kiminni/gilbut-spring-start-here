f0560b4fdc717d235185e3cce88d0256432bdea7
Kiminni <imk0980@gmail.com>
2024-11-30 03:44:33 +0900

    "4-4 @Bean을 통한 의존성 주입"

--- sq-ch4-ex4/src/main/java/configuration/ProjectConfiguration.java
+++ sq-ch4-ex4/src/main/java/configuration/ProjectConfiguration.java
@@ -9,9 +9,13 @@ import repositories.CommentRepository;
 import repositories.DBCommentRepository;
 import services.CommentService;
 
-@Configuration
+@Configuration //@ComponentScan은 스테레오 타입 애너테이션을 쓰지 않기에 필요가 없어졌다.
 public class ProjectConfiguration {
 
+  /**
+   * @Bean을 통해 매개변수로 오토 와이어링을 사용한다.
+   * 각각의 두 의존성에 직접 설정한다.
+   */
   @Bean
   public CommentRepository commentRepository() {
     return new DBCommentRepository();
@@ -22,7 +26,7 @@ public class ProjectConfiguration {
     return new EmailCommentNotificationProxy();
   }
 
-  @Bean
+  @Bean //@Bean 을 통해 스프링 컨텍스트에서 호환되는 빈에 대한 참조를 제공한다.
   public CommentService commentService(CommentRepository commentRepository,
                                        CommentNotificationProxy commentNotificationProxy) {
     return new CommentService(commentRepository, commentNotificationProxy);
