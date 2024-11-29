15a43ff11f12c2314bb8c6e3e4d9394028d7a6eb
Kiminni <imk0980@gmail.com>
2024-11-30 03:48:16 +0900

    "4-5 @Primary를 통한 우선순위 결정"

--- sq-ch4-ex5/src/main/java/proxies/CommentPushNotificationProxy.java
+++ sq-ch4-ex5/src/main/java/proxies/CommentPushNotificationProxy.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 // @Primary가 없다면 NoUniqueBeanDefinitionException 이 발생한다.
+// @Primary는 여러 빈 중에서 우선순위를 정할 때 사용한다.
+// 특정 인터페이스에 대한 구현을 제공하는 인터페이스를 사용하나, 어느순간 맞아 떨어지지 않는다면 가장 간단하게 @Primary를 사용할 수 있다.
 @Primary
 public class CommentPushNotificationProxy
         implements CommentNotificationProxy {
