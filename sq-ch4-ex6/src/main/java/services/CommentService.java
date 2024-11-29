913259500b897ffbc2b91d7a1c7cd55873947c7c
Kiminni <imk0980@gmail.com>
2024-11-30 03:50:10 +0900

    "4-5 @Qualifier를 통한 우선순위 결정"

--- sq-ch4-ex6/src/main/java/services/CommentService.java
+++ sq-ch4-ex6/src/main/java/services/CommentService.java
@@ -6,13 +6,16 @@ import org.springframework.stereotype.Component;
 import proxies.CommentNotificationProxy;
 import repositories.CommentRepository;
 
+// @Qualifier을 통해 CommentNotificationProxy의 구현체를 선택할 수 있다.
+// @Qualifier("PUSH")를 통해 CommentPushNotificationProxy를 사용한다.
+// @Qualifier("EMAIL")을 통해 EmailCommentNotificationProxy를 사용한다.
+
 @Component
 public class CommentService {
 
   private final CommentRepository commentRepository;
 
   private final CommentNotificationProxy commentNotificationProxy;
-
   public CommentService(CommentRepository commentRepository,
                         @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
     this.commentRepository = commentRepository;
